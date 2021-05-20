e11fab47eff9275434556c98d5a467cd8dd13060
Antoine07 <antoine.lucsko@gmail.com>
2021-05-20 16:53:37 +0200

    revision 03

--- revisions/revision_03/Ex_02/src/app.js
+++ revisions/revision_03/Ex_02/src/app.js
@@ -1,27 +1,49 @@
-import { hello } from './utils.js';
+import { createEl, checkNumber, parityClass } from "./utils.js";
 
+window.addEventListener("DOMContentLoaded", (event) => {
+  const elNumber = document.getElementById("number");
+  const btnSubmit = document.getElementById("btn_submit");
+  const elNumbers = document.querySelector(".numbers");
+  const elMessage = document.querySelector(".message");
 
-window.addEventListener('DOMContentLoaded', event => {
-    
-    const number = document.getElementById('number');
-    const btnSubmit = document.getElementById('btn_submit');
-    const numbers = document.querySelector('.numbers');
+  let number = "";
 
-    let value = '';
+  const handleChange = (event) => {
+    const { value } = event.target;
+    elMessage.innerHTML = "";
+    number = value;
+  };
 
-    const handleChange = event => {
-        const { value : number } = event.target; 
+  elNumber.addEventListener("input", handleChange);
 
-        console.log(number);
+  elNumber.addEventListener("focus", function (e) {
+    e.target.value = "";
+    elMessage.innerHTML = "";
+  });
 
-        value = number;
-    };
+  btnSubmit.addEventListener("click", (event) => {
+    const { message, flag } = checkNumber(elNumber.value);
 
-    number.addEventListener('input', handleChange );
+    if (flag === false) {
+      elMessage.innerHTML = message;
 
-    btnSubmit.addEventListener('click', event => {
-        // Create element li et l'ajouter dans numbers ul 
+      return;
+    }
 
+    const li = createEl({ name: "li", content: number });
+    li.setAttribute('style', parityClass(number).join('; ')) ;
+    const button = createEl({ name : 'button', content : 'X'});
+
+    button.addEventListener('click', event => {
+        li.remove();
     });
 
+    const classes = ['btn', 'btn-danger', 'cursor'];
+    button.classList.add(...classes); // spread => propagé les valeurs du tableau en paramètre de la fonction add
+
+    li.appendChild(button);
+    document.body.insertBefore(li, elNumbers.nextElementSibling);
+    elNumber.value = "";
+    elMessage.innerHTML = message;
+  });
 });
--- revisions/revision_03/Ex_02/src/utils.js
+++ revisions/revision_03/Ex_02/src/utils.js
@@ -1,3 +1,36 @@
 
+const styleNormal = [
+    'color: black',
+    'margin-top: .5rem'
+];
 
-export const hello = "Hello";
+const styleOrange = [
+    'color: orange',
+    'margin-top: .5rem'
+];
+
+export const createEl = ({name, content}) => {
+    const el = document.createElement(name);
+    const newContent = document.createTextNode(content);
+
+    el.appendChild(newContent);
+
+    return el;
+}
+
+export const checkNumber = (number) => {
+
+    if( number.trim() ==='' ) return (
+         { message : "Attention votre champ est vide", flag : false }
+    );
+
+    if( isNaN(parseFloat(number))) return ( 
+        { message : `Attention ce n'est pas un nombre ${number}`, flag : false }
+    );
+
+    return ( 
+        { message : `Merci pour votre ajout ${number}`, flag : true }
+    );
+}
+
+export const parityClass = number => number % 2 ? styleNormal : styleOrange ;
